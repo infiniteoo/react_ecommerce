485a9b51b1532eb9984c5a97d9f189116a95e86f
Troy Dorman <troydorman@gmail.com>
2021-03-28 12:18:24 -0600

    successfully retrieving and rendering product data from the commerce.js api

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import {Grid} from '@material-ui/core';
 import Product from './Product/Product';
+import macbookImage from '../../imgs/macbook.jpg'
+import shoeImage from '../../imgs/shoes.jpg'
 
-const products = [
-
-    { id: 1, name: 'Shoes', description: 'Running shoes.', price: "$5"},
-    { id: 2, name: 'Macbook', description: 'Apple macbook.', price: "$10"},
+import useStyles from './productsStyles';
 
 
-];
 
-const Products = () => {
+const Products = ({ products }) => {
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
         <Grid container justify='center' spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
