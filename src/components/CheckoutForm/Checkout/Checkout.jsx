da702195d1665ea5e58593af8c6f04bf84a73349
Troy Dorman <troydorman@gmail.com>
2021-03-31 11:01:35 -0600

    integrated stripe payment processing, it appears to work!

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -10,7 +10,7 @@ import PaymentForm from '../PaymentForm';
 
 const steps = ['Shipping address', 'Payment details'];
 
-const Checkout = ({ cart }) => {
+const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
     const [activeStep, setActiveStep] = useState(0);
     const [shippingData, setshippingData] = useState({})
     const [checkoutToken, setcheckoutToken] = useState(null);
@@ -49,7 +49,7 @@ const Checkout = ({ cart }) => {
 
     const Form = () => activeStep === 0
     ? <AddressForm checkoutToken={checkoutToken} next={next}/>
-    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken}/>
+    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} onCaptureCheckout={onCaptureCheckout} nextStep={nextStep} backStep={backStep}/>
 
     
 
